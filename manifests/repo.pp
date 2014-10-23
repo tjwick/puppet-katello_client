5a72d45a9191b0afa51e81aec4cb092db0a32747
Craft--- <ianjhenry00@gmail.com>
2014-10-23 16:34:16 -0400

    Update repo.pp

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,7 +1,8 @@
 class katello_client::repo {
+
 ### Repo config, using case to allow for future support of SUSE or Debian ###
 
-  case $::osfamily{   
+  case $::osfamily{
     'RedHat', 'Linux':{
       yumrepo { 'sub-manager':
         descr      => 'An open source entitlement management system',
@@ -9,20 +10,21 @@ class katello_client::repo {
         enabled    => 1,
         gpgcheck   => 0,
     }
-
-    if $manage_repo and $agent_install {
-      yumrepo { 'katello':
-        descr       => 'Repo for katello tools and software',
-        baseurl     => "http://fedorapeople.org/groups/katello/releases/yum/2.0/katello/RHEL/${::operatingsystemmajrelease}Server/${::architecture}/katello-repos-latest.rpm
+    if $katello_client::manage_repo and $katello_client::agent_install {
+      yumrepo { 'katello-pulp':
+        descr       => 'Pulp Community Releases',
+        baseurl     => "http://fedorapeople.org/groups/katello/releases/yum/2.0/pulp/RHEL/${::operatingsystemmajrelease}Server/${::architecture}/",
         enabled     => 1,
-        gpgcheck    => 0,
+        gpgkey      => 'http://www.katello.org/gpg/RPM-GPG-KEY-katello-2012.gpg',
+        gpgcheck    => 1,
         require     => Yumrepo['sub-manager'],
+      }
+    }
+  }
+    default: {
+      fail("katello-client provides no repository information for OSfamily ${::osfamily}")
     }
   }
 
-    default: {
-      fail("\"${module_name}\" provides no repository information for OSfamily \"${::osfamily}\"")
-    }
-  }
 }
 
