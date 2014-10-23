f8d19700da8684c16209bc30424e28bec01c6da5
Craft--- <ianjhenry00@gmail.com>
2014-10-23 16:33:48 -0400

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,34 +1,37 @@
 class katello_client::package {
 
  ####Package Management#####
-  if $katello_agent == true {
-  
-    if $katello_client::version == 'latest' {
-      
-      if $katello_client::subman_version == 'latest' {
-        package {'subscription-manager':
-          ensure  => latest,
-          require => Yumrepo['sub-manager'],
-      } else {
-        package { 'subscription-manager':
-          ensure  => "$katello_client::subman_version",
-          require => Yumrepo['sub-manager'],
-        }
-      }
+  if $katello_client::agent_install == true {
+
+    if $katello_client::version == latest {
+
       package { 'katello-agent':
         ensure    => latest,
-        require   => Yumrepo['katello'],
+        require   => Yumrepo['katello-pulp'],
+      }
     } else {
       package { 'katello-agent':
         ensure    => "$katello_client::version",
-        require   => Yumrepo['katello'],
-      }    
+        require   => Yumrepo['katello-pulp'],
+      }
     }
-  } else {
-    if $katello_client::subman_version == 'latest' {
+      if $katello_client::subman_version == latest {
         package {'subscription-manager':
           ensure  => latest,
           require => Yumrepo['sub-manager'],
+        }
+      } else {
+        package { 'subscription-manager':
+          ensure  => "$katello_client::subman_version",
+          require => Yumrepo['sub-manager'],
+        }
+      }
+    } else {
+    if $katello_client::subman_version == latest {
+        package {'subscription-manager':
+          ensure  => latest,
+          require => Yumrepo['sub-manager'],
+        }
       } else {
         package { 'subscription-manager':
           ensure  => "$katello_client::subman_version",
@@ -36,6 +39,5 @@ class katello_client::package {
         }
       }
     }
-    fail("Katello-client version must be latest, or valid version number, you have provided \"${katello_client::version}\"" 
-  }
+#  fail("Katello-client version must be latest, or valid version number, you have provided \"${katello_client::version}\"")
 }
