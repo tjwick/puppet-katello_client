cb4bc174e1ea53134a7a67fb32ef9f612b27c73b
Cristian Falcas <cristi.falcas@gmail.com>
2016-03-21 10:35:42 +0200

    package shouldn't depend on yum repos

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,17 +1,11 @@
 class katello_client::package {
   ####Package Management#####
+
   if $katello_client::agent_install == true {
-    package { 'katello-agent':
-      ensure  => $katello_client::version,
-      require => Yumrepo['katello-pulp'],
-    }
+    package { 'katello-agent': ensure => $katello_client::version, }
   }
 
   if $katello_client::subman_install {
-    package { 'subscription-manager':
-      ensure  => "$katello_client::subman_version",
-      require => Yumrepo['sub-manager'],
-    }
+    package { 'subscription-manager': ensure => "$katello_client::subman_version", }
   }
-  # fail("Katello-client version must be latest, or valid version number, you have provided \"${katello_client::version}\"")
 }
